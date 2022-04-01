5c8bb052a58301d0f1ec6157ae9d4681aca2a98d
k1raaa123 <zivilsk0573@mail.ru>
2022-04-01 20:56:31 +0300

    finally5

--- main/views.py
+++ main/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'main/Главная.html')
+    return render(request, 'main/Glavnaya.html')
 
 
 def categories(request):
-    return render(request, 'main/Категории.html')
+    return render(request, 'main/Category.html')
 
 
 def create(request):
-    return render(request, 'main/Заказ.html')
+    return render(request, 'main/Zakaz.html')
