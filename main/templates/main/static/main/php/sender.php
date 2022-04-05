4f9185d9f2f2b761d9635b459d901cdc338d6160
k1raaa123 <zivilsk0573@mail.ru>
2022-04-05 21:19:59 +0300

    comments_upd

--- main/templates/main/static/main/php/sender.php
+++ main/templates/main/static/main/php/sender.php
@@ -3,12 +3,14 @@
 	$phone = $_POST['phone'];
     $email = $_POST['email'];
     $text = $_POST['text'];
+    //создание сообщения
 
 	$to = "zivilsk@mail.ru";
 	$date = date ("d.m.Y"); 
 	$time = date ("h:i");
 	$from = $email;
 	$subject = "Заявка c сайта";
+	//доп информация в сообщении
 
 	
 	$msg="
@@ -17,6 +19,7 @@
     Почта: $email /n
     Текст: $text"; 	
 	mail($to, $subject, $msg, "From: $from ");
+	//отправка сообщения
 
 ?>
 
